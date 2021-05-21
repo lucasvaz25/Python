1d03bf6da7bf421dc3828c7a100ed6d540edd66d
unknown <lucas.vaz8@gmail.com>
2021-05-21 01:02:35 -0300

    master

--- ProvaWeb/provaWeb/settings.py
+++ ProvaWeb/provaWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-ezprf4sb%@$9c_c9z)c+_7ek@b(l$73varrpc5c^=h1xlt(hj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
